156f1a06f75ebf3babde315fff35fb4c18cd03a9
Mayank Kr <ikki2@live.com>
2020-04-18 21:01:18 +0530

    Update euler25.c

--- C/1-25/euler25.c
+++ C/1-25/euler25.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
-int digits(unsigned long long n) {
+int digits(unsigned n) {
     int l_n = (n * log10((1 + sqrt(5)) / 2)) - (log10(5) / 2);
 
     return floor(l_n) + 1;
 }
 
 int main() {
-    unsigned long long n = 8;
+    unsigned n = 1;
     
     while(1) {
         if(digits(n) == 1000) {
@@ -17,7 +17,7 @@ int main() {
         ++n;
     }
 
-    printf("%llu\n", n);
+    printf("%u\n", n);
     
     return 0;
 }
